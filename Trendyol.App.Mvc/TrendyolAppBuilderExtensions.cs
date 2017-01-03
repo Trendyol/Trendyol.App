db984493d4a1db79319e67f3e867c4c53b26c64b
Ömer Cinbat <ocinbat@outlook.com>
2017-01-03 22:19:23 +0300

    Added support for bundle config.

--- Trendyol.App.Mvc/TrendyolAppBuilderExtensions.cs
+++ Trendyol.App.Mvc/TrendyolAppBuilderExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace Trendyol.App.Mvc
 {
     public static class TrendyolAppBuilderExtensions
     {
-        public static TrendyolAppBuilder UseMvc(this TrendyolAppBuilder builder, Action<RouteCollection> registerRoutesAction = null)
+        public static TrendyolAppBuilder UseMvc(this TrendyolAppBuilder builder, Action<RouteCollection> registerRoutesAction = null, Action<BundleCollection> registerBundles = null)
         {
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
@@ -14,6 +15,7 @@ namespace Trendyol.App.Mvc
             RouteTable.Routes.LowercaseUrls = true;
             RouteTable.Routes.MapMvcAttributeRoutes();
             registerRoutesAction?.Invoke(RouteTable.Routes);
+            registerBundles?.Invoke(BundleTable.Bundles);
 
             return builder;
         }
