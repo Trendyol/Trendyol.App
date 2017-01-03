21bd3eb61360688499a5fe0477ae8d032adecaef
Ömer Cinbat <ocinbat@outlook.com>
2017-01-03 21:13:03 +0300

    Added html helper extension class.

---
+++ Trendyol.App.Mvc/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Trendyol.App.Domain.Dtos;
+
+namespace Trendyol.App.Mvc
+{
+    public static class HtmlHelperExtensions
+    {
+        public static SelectList ToSelectList(this IEnumerable<LookupDto> lookups)
+        {
+            return new SelectList(lookups, "Value", "Text");
+        }
+
+        public static SelectList ToSelectList(this IEnumerable<LookupDto> lookups, object selectedValue)
+        {
+            return new SelectList(lookups, "Value", "Text", selectedValue);
+        }
+    }
+}
