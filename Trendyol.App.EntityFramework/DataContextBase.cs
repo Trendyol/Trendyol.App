8df70567177541536d661b7bdb299d95dc398b25
Ömer Cinbat <ocinbat@outlook.com>
2017-03-07 11:29:46 +0300

    Movec GetNextId to DataContext.

--- Trendyol.App.EntityFramework/DataContextBase.cs
+++ Trendyol.App.EntityFramework/DataContextBase.cs
@@ -4,9 +4,11 @@ using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Infrastructure.Interception;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using EntityFramework.Filters;
 using Trendyol.App.Data;
+using Trendyol.App.Data.Attributes;
 
 namespace Trendyol.App.EntityFramework
 {
@@ -73,6 +75,31 @@ namespace Trendyol.App.EntityFramework
             }
 
             return base.SaveChanges();
+        }
+
+        public TId GetNextId<TEntity, TId>() where TEntity : IEntity<TId>
+        {
+            Type entityType = typeof(TEntity);
+
+            SequenceAttribute attribute = entityType.GetCustomAttribute<SequenceAttribute>();
+
+            if (attribute == null)
+            {
+                throw new InvalidOperationException("You need to decorate your entity with Sequence attribute to use this extension method.");
+            }
+
+            string sequenceName = attribute.Name;
+
+            DbRawSqlQuery<TId> result = Database.SqlQuery<TId>($"SELECT (NEXT VALUE FOR {sequenceName})");
+
+            TId id = result.FirstOrDefault();
+
+            if (id == null || id.Equals(default(TId)))
+            {
+                throw new InvalidOperationException($"Database did not return an instance of identity for sequence {sequenceName}.");
+            }
+
+            return id;
         }
     }
 }
