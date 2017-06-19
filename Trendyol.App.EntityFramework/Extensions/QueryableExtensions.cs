c8b435e3280e27b2583b63930cd4553dcc3c53e9
Ömer Cinbat <ocinbat@outlook.com>
2017-06-19 17:02:53 +0300

    Added NoLock support to EF.

--- Trendyol.App.EntityFramework/Extensions/QueryableExtensions.cs
+++ Trendyol.App.EntityFramework/Extensions/QueryableExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Dynamic;
+using EntityFramework.InterceptorEx;
 using Trendyol.App.Domain.Abstractions;
 using Trendyol.App.Domain.Enums;
 using Trendyol.App.Domain.Objects;
@@ -17,12 +18,16 @@ namespace Trendyol.App.EntityFramework.Extensions
         {
             if (source == null)
             {
-                throw new ArgumentNullException($"You cannot pageinate on a null object reference. The parameter source should be initialized.", nameof(source));
+                throw new ArgumentNullException(
+                    $"You cannot pageinate on a null object reference. The parameter source should be initialized.",
+                    nameof(source));
             }
 
             if (request == null)
             {
-                throw new ArgumentNullException($"You need to initialize a paging request before paging on a list. The parameter request should be initialized.", nameof(request));
+                throw new ArgumentNullException(
+                    $"You need to initialize a paging request before paging on a list. The parameter request should be initialized.",
+                    nameof(request));
             }
 
             if (request.Page == 0)
@@ -44,7 +49,8 @@ namespace Trendyol.App.EntityFramework.Extensions
 
             if (String.IsNullOrEmpty(request.OrderBy))
             {
-                throw new InvalidOperationException($"In order to use paging extensions you need to supply an OrderBy parameter.");
+                throw new InvalidOperationException(
+                    $"In order to use paging extensions you need to supply an OrderBy parameter.");
             }
 
             if (request.Order == OrderType.Asc)
@@ -56,16 +62,31 @@ namespace Trendyol.App.EntityFramework.Extensions
                 source = source.OrderBy(request.OrderBy + " descending");
             }
 
-            int skip = (request.Page - 1) * request.PageSize;
+            int skip = (request.Page - 1)*request.PageSize;
             int take = request.PageSize;
             int totalItemCount = source.Count();
 
             return new Page<T>(source.Skip(skip).Take(take), request.Page, request.PageSize, totalItemCount);
         }
 
-        public static IFilteredExpressionQuery<TResult> Select<TResult>(this IQueryable<TResult> source, string fields) where TResult : class
+        public static IFilteredExpressionQuery<TResult> Select<TResult>(this IQueryable<TResult> source, string fields)
+            where TResult : class
         {
             return new FilteredExpressionQuery<TResult>(source.AsNoTracking(), fields);
+        }
+
+        public static TResult WithNoLock<T, TResult>(this IQueryable<T> query, Func<IQueryable<T>, TResult> action)
+        {
+            try
+            {
+                WithNoLockInterceptor.Suppress = false;
+                TResult queryableResult = action(query);
+                return queryableResult;
+            }
+            finally
+            {
+                WithNoLockInterceptor.Suppress = true;
+            }
         }
     }
 }
