64b85160a65518c68ffafe5349744d31db10a726
Ömer Cinbat <ocinbat@outlook.com>
2017-06-24 12:52:13 +0300

    Added support for auto migrations with ef.

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Data.SampleDataContext>
+    public sealed class Configuration : DbMigrationsConfiguration<Data.SampleDataContext>
     {
         public Configuration()
         {
---
+++ Trendyol.App.EntityFramework/TrendyolAppBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity.Migrations;
+
+namespace Trendyol.App.EntityFramework
+{
+    public static class TrendyolAppBuilderExtensions
+    {
+        public static TrendyolAppBuilder UseAutomaticMigrations<T>(this TrendyolAppBuilder builder) where T : DbMigrationsConfiguration
+        {
+            builder.AfterBuild(() =>
+            {
+                T configuration = Activator.CreateInstance<T>();
+                DbMigrator migrator = new DbMigrator(configuration);
+                migrator.Update();
+            });
+
+            return builder;
+        }
+    }
+}
