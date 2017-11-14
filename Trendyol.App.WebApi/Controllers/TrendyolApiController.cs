008718b4369393e3d0afde9c0ef824e832ef6e28
Selman Ahatlı <selman.ahatli@trendyol.com>
2017-11-14 15:37:27 +0300

    Add Access-Control-Expose-Headers to TrendyolApiController

--- Trendyol.App.WebApi/Controllers/TrendyolApiController.cs
+++ Trendyol.App.WebApi/Controllers/TrendyolApiController.cs
@@ -61,6 +61,7 @@ namespace Trendyol.App.WebApi.Controllers
             response.Headers.Add("X-Paging-HasPreviousPage", page.HasPreviousPage.ToString().ToLowerInvariant());
             response.Headers.Add("X-Paging-HasNextPage", page.HasNextPage.ToString().ToLowerInvariant());
             response.Headers.Add("Link", GetLinkHeaderForPageResult(page));
+            response.Headers.Add("Access-Control-Expose-Headers", "X-Paging-TotalCount, X-Paging-TotalPages");
 
             return ResponseMessage(response);
         }
